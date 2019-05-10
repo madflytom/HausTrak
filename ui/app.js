127d98dff6e1779272f59e3c1137422000abe74e
Thomas Stahl <thomasstahl@Thomass-Air.attlocal.net>
2019-05-10 06:58:30 -0400

    removing unused code

--- ui/app.js
+++ ui/app.js
@@ -1,28 +1,12 @@
 new Vue({
     el: "#app",
     data: {
-        title: 'TODOS',
-        todos: [
-            {text: 'todo 1', done: true, id: Date.now()},
-            {text: 'todo 2', done: false, id: Date.now() + 1}
-        ],
         projectItems: [
         ],
         projects: [
         ]
     },
     methods: {
-        addTodo(event) {
-            const text = event.target.value
-            this.todos.push({text, done: false, id: Date.now()})
-            event.target.value = ''
-        },
-        removeTodo(id) {
-            this.todos = this.todos.filter(todo => todo.id !== id)
-        },
-        check(todo){
-            todo.done = !todo.done
-        },
         addProjectItem(event) {
             const title = event.target.value
             axios.post("http://localhost:5000/post_projectItem", {
