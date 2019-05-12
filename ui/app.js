91503e79ef94b56675aced9a74e2fcda74b78acc
Thomas Stahl <thomasstahl@Thomass-Air.attlocal.net>
2019-05-11 21:14:54 -0400

    figuring out how to separate componets

--- ui/app.js
+++ ui/app.js
@@ -1,4 +1,51 @@
-new Vue({
+// Define a new component called button-counter
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  })
+
+Vue.component('project-item-component', {
+    props: ['projectItem'],
+    filters: {},
+    methods: {
+        checkProjectItem(projectItem){
+            done = !projectItem.done
+            axios.post("http://localhost:5000/check_projectItem/" + projectItem.id, {
+                done: done
+            }).then(response => {
+                projectItem.done = done
+            });
+        },
+        deleteProjectItem(id){
+            axios.delete("http://localhost:5000/delete_projectItem/" + id).then(response => 
+            {
+                //TODO: Tell app.js to remove the item?
+                this.$parent.getProjectItems();
+            });
+        }
+    },
+    template: `<div>
+        <ul> 
+            <li class="flex"> 
+                <label>
+                    <input type="checkbox" class="nes-checkbox" v-model="projectItem.done" v-on:click="checkProjectItem(projectItem)">
+                    <span>&nbsp</span>
+                </label> 
+                <del v-if="projectItem.done">{{ projectItem.title }}</del>
+                <span v-else>{{ projectItem.title }}</span>
+                <div class="space"></div>
+                <button class="nes-btn is-error padding" v-on:click="deleteProjectItem(projectItem.id)">X</button>
+            </li>
+        </ul>
+    </div>`
+})
+
+
+const app = new Vue({
     el: "#app",
     data: {
         projectItems: [
@@ -21,21 +68,9 @@ new Vue({
                 event.target.value = ''
             });
         },
-        checkProjectItem(projectItem){
-            done = !projectItem.done
-            axios.post("http://localhost:5000/check_projectItem/" + projectItem.id, {
-                done: done
-            }).then(response => {
-                projectItem.done = done
-            });
-        },
-        deleteProjectItem(id){
-            axios.delete("http://localhost:5000/delete_projectItem/" + id).then(response => 
-            {
-                //TODO: maybe return the updated list of project items?
-                axios.get("http://localhost:5000/projectItem").then(response => {
-                    this.projectItems = response.data;
-                });
+        getProjectItems(){
+            axios.get("http://localhost:5000/projectItem").then(response => {
+                this.projectItems = response.data;
             });
         }
     },
@@ -43,8 +78,22 @@ new Vue({
         axios.get("http://localhost:5000/project").then(response => {
           this.projects = response.data;
         });
-        axios.get("http://localhost:5000/projectItem").then(response => {
-            this.projectItems = response.data;
-        });
-    }
+        this.getProjectItems();
+    },
+    template: `
+        <div>
+            <div>
+                <h1>Some other stuff</h1>
+                <ul> 
+                    <project-item-component v-for="projectItem in projectItems" v-bind:projectItem="projectItem" />
+                </ul>
+            </div>
+            <input type="text" class="nes-input" placeholder="Add project item..." v-on:keyup.enter="addProjectItem">
+
+            <div id="components-demo">
+                <button-counter></button-counter>
+            </div>
+        </div>
+    `
 })
+
